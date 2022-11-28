8665bb1cebeb428e0e194e4c3d0ac39c773a9655
jarenella <jamesa122333@gmail.com>
2022-11-27 18:53:24 -0800

    profile pages now render users following and follower count

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Follower } = require('../models');
 const authLogIn = require('../utils/authLogIn');
 
 router.get('/', authLogIn, async (req, res) => {
@@ -21,8 +21,24 @@ router.get('/profile', authLogIn, async (req, res) => {
     const userData = await User.findByPk(req.session.user_id, {include: [{model: Post}]})
     const user = userData.get({plain:true})
 
+    //get how many followers they have
+    const followers = await Follower.findAndCountAll({
+        where: {
+            followed_user_id: user.id
+        }
+    });
+    const followerCount = followers.count;
+
+    //get how many people they are following
+    const following = await Follower.findAndCountAll({
+        where: {
+            following_user_id: user.id
+        }
+    });
+    const followingCount = following.count;
+
     //... spreads the user data?
-    res.render("profile", {...user, logged_in:req.session.logged_in});
+    res.render("profile", {...user, logged_in:req.session.logged_in, followerCount, followingCount});
 })
 
 router.get('/post/:id', authLogIn, async (req, res) => {
@@ -39,11 +55,29 @@ router.get('/post/:id', authLogIn, async (req, res) => {
 
 router.get('/user/:id', async (req, res) => {
     const userData = await User.findByPk(req.params.id,  {include: [{model: Post}]})
-    console.log(userData);
     
-    if (userData) { //if a user with that id exists, render it into the post page
-        const user = userData.get({plain:true})
-        res.render("user", {...user, logged_in:req.session.logged_in});
+    if (userData) { //if a user with that id exists, render it into the user page
+        const user = userData.get({ plain: true })
+
+        //get how many followers they have
+        const followers = await Follower.findAndCountAll({
+            where: {
+                followed_user_id: user.id
+            }
+        });
+        const followerCount = followers.count;
+        console.log(followerCount + " is the follower count")
+
+        //get how many people they are following
+        const following = await Follower.findAndCountAll({
+            where: {
+                following_user_id: user.id
+            }
+        });
+        const followingCount = following.count;
+        console.log(followingCount + " is the following count")
+
+        res.render("user", { ...user, logged_in: req.session.logged_in, followerCount, followingCount });
     }
     else { //if a user with that id doesnt exist, render the 404 page
         res.render("404route", {logged_in:req.session.logged_in});
